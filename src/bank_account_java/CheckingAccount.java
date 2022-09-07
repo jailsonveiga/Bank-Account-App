2224b0cb933d2a0db08b0de3a7a082216301ac91
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-06 20:23:10 -0400

    Update

--- src/bank_account_java/CheckingAccount.java
+++ src/bank_account_java/CheckingAccount.java
@@ -1,4 +1,65 @@
 package bank_account_java;
 
-public class CheckingAccount {
+// Checking Account
+public class CheckingAccount  extends Account{
+
+    // Default transaction Fee
+    private static double FEE = 2.5;
+
+    // Default constructor
+    public CheckingAccount() {
+        super();
+    }
+
+    /**
+     * Parameter constructor to initialize CheckingAccount
+     * with a custom Account Number and a Customer Transaction Fee
+     */
+    public CheckingAccount (int accountNumber, double fee) {
+        super(accountNumber);
+        FEE = fee;
+    }
+    /**
+     * Function to deposit funds into the account as long as the amount parameter is > 0
+     *
+     * Apply Transaction fee for the CheckingAccount
+     * @param amount is value to be deposited
+     */
+    public void deposit(double amount) {
+
+        // First Check amount
+        if (amount > 0) {
+            balance += amount;
+            System.out.printf("Amount %.2f deposited%n", amount);
+            // Apply Transaction Fee
+            balance -= FEE;
+            System.out.printf("Fee %.2f Applied%n", FEE);
+            System.out.printf("Current Balance is: %.2f%n", balance);
+        } else {
+            System.out.println("A negative amount cannot be deposited");
+        }
+    }
+
+    /**
+     * function to widraw funds from the Account as long as
+     * 1. Amount to withdraw must be > 0
+     * 2. Amount to withdraw must be <= balance
+     * @param amount value to be withdrawn
+     */
+    public void withdraw(double amount){
+
+        // Same check
+        if (amount > 0) {
+            // Check sufficient balance
+            if((amount + FEE) <= balance) {
+                System.out.printf("Amount %.2f withdrawn from Account%n", amount);
+                balance -= amount;
+                balance -= FEE;
+                System.out.printf("Fee %.2f Applied%n", FEE);
+                System.out.printf("Current Balance is: %.2f%n", balance);
+            } else {
+                System.out.println("Negative amount cannot be withdrawn");
+            }
+        }
+    }
 }
