0ef95ac0f4184ba619e466ce7d4e01c3656b050a
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-06 20:23:14 -0400

    Update

--- src/bank_account_java/SavingsAccount.java
+++ src/bank_account_java/SavingsAccount.java
@@ -1,4 +1,85 @@
 package bank_account_java;
 
-public class SavingsAccount {
+// Savings Account child class
+// has an interest rate
+// a method to apply interest - profit
+public class SavingsAccount extends Account {
+
+    // interest rate
+    private double interestRate;
+
+    // Default constructor
+    public SavingsAccount() {
+        super();
+    }
+
+    /**
+     * Parameter constructor to initialize Savings account
+     * with a customer account number and interest rate
+     */
+    public SavingsAccount(int accountNumber, double interestRate) {
+        super(accountNumber);
+        this.interestRate = interestRate;
+    }
+
+    // Getter function
+    public double getInterestRate() {
+        return this.interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double calcInterest() {
+        return balance * interestRate;
+    }
+
+    public void applyInterest() {
+       double interest = calcInterest();
+       System.out.printf("Interest amount %.2f added to balance", interest);
+       deposit(interest);
+    }
+
+    /**
+     * Function to deposit funds into the account as long as the amount parameter is > 0
+     * <p>
+     * Apply Transaction fee for the CheckingAccount
+     *
+     * @param amount is value to be deposited
+     */
+    public void deposit(double amount) {
+
+        // First Check amount
+        if (amount > 0) {
+            balance += amount;
+            System.out.printf("Amount %.2f deposited%n", amount);
+
+            System.out.printf("Current Balance is: %.2f%n", balance);
+        } else {
+            System.out.println("A negative amount cannot be deposited");
+        }
+    }
+
+    /**
+     * function to widraw funds from the Account as long as
+     * 1. Amount to withdraw must be > 0
+     * 2. Amount to withdraw must be <= balance
+     *
+     * @param amount value to be withdrawn
+     */
+    public void withdraw(double amount) {
+
+        // Same check
+        if (amount > 0) {
+            // Check sufficient balance
+            if ((amount) <= balance) {
+                System.out.printf("Amount %.2f withdrawn from Account%n", amount);
+                balance -= amount;
+                System.out.printf("Current Balance is: %.2f%n", balance);
+            } else {
+                System.out.println("Negative amount cannot be withdrawn");
+            }
+        }
+    }
 }
